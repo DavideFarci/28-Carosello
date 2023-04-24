6b8415bace4a29892cbb8ef7c1ef9a9209414587
DavideFarci <yank92.df@gmail.com>
2023-04-24 23:45:11 +0200

    add html with javascript

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,18 @@
 /*
 Quando l'utente preme uno dei bottoni, la classe active passa da un'immagine all'altra 
 */
+const arrImages = [
+    `assets/img/01.jpg`,
+    `assets/img/02.jpg`,
+    `assets/img/03.jpg`,
+    `assets/img/04.jpg`,
+    `assets/img/05.jpg`,
+]
+const containerHighlighted = document.querySelector(".highlighted");
+
+for (let i = 0; i < arrImages.length; i++) {
+    containerHighlighted.innerHTML += `<img src="${arrImages[i]}" alt="" class="${i == 0 ? "active" : ""}">`;
+}
 
 // seleziono le immagini nell'html
 const listHighlighted = document.querySelectorAll(".highlighted img");
